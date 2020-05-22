0a1c795a86ad36c7acced602525c95bd55958da0
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-05-22 11:43:49 -0700

    pass third test for projects

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,20 +3,20 @@ require "spec_helper"
 describe (Project) do
 
   describe('#title') do
-    it ('returns the project title') do
+    it('returns the project title') do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
       expect(project.title).to(eq('Teaching Kids to Code'))
     end
   end
 
 
-  describe ('#id') do
-    it ('returns the id of the project before saving project') do
+  describe('#id') do
+    it('returns the id of the project before saving project') do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
       expect(project.id).to(eq(0))
     end
 
-    it ('returns the id of the project after saving project') do
+    it('returns the id of the project after saving project') do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
       project.save
       expect(project.id).to(be_an_instance_of(Integer))
@@ -24,13 +24,13 @@ describe (Project) do
   end
 
 
-#   describe('#==') do
-#     it 'is the same project if two projects have the same title' do
-#       project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       expect(project1 == project2).to(eq(true))
-#     end
-#   end
+  describe('#==') do
+    it('is the same project if two projects have the same title') do
+      project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      expect(project1 == project2).to(eq(true))
+    end
+  end
 #   #
 #   context '.all' do
 #     it 'is empty to start' do
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -3,21 +3,21 @@ require "spec_helper"
 describe Volunteer do
 
   describe('#name') do
-    it ('returns the name of the volunteer') do
+    it('returns the name of the volunteer') do
       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       expect(test_volunteer.name).to(eq('Jane'))
     end
   end
 
   describe('#project_id') do
-    it 'returns the project_id of the volunteer' do
+    it('returns the project_id of the volunteer') do
       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       expect(test_volunteer.project_id).to(eq(1))
     end
   end
 
   describe('#==') do
-    it ('checks for equality based on the name of a volunteer') do
+    it('checks for equality based on the name of a volunteer') do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       expect(volunteer1 == volunteer2).to(eq(true))
@@ -25,11 +25,11 @@ describe Volunteer do
   end
 
   context('.all') do
-    it ('is empty to start') do
+    it('is empty to start') do
       expect(Volunteer.all).to(eq([]))
     end
 
-    it ('returns all volunteers') do
+    it('returns all volunteers') do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       volunteer1.save
       volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
@@ -39,7 +39,7 @@ describe Volunteer do
   end
 
   describe('#save') do
-    it ('adds a volunteer to the database') do
+    it('adds a volunteer to the database') do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       volunteer1.save
       expect(Volunteer.all).to(eq([volunteer1]))
@@ -47,7 +47,7 @@ describe Volunteer do
   end
 
   describe('.find') do
-    it ('returns a volunteer by id') do
+    it('returns a volunteer by id') do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       volunteer1.save
       volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
@@ -57,7 +57,7 @@ describe Volunteer do
   end
 
   describe('.delete') do
-    it ('deletes a volunteer by id') do
+    it('deletes a volunteer by id') do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       volunteer1.save
       volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
